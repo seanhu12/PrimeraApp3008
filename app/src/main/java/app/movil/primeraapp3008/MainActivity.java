7b2597a8b9e818c520ef77daf633355ddb79a21d
seanhu12 <sebastian.huanca@alumnos.ucn.cl>
2022-08-31 12:16:52 -0400

    ciclo de vida

--- app/src/main/java/app/movil/primeraapp3008/MainActivity.java
+++ app/src/main/java/app/movil/primeraapp3008/MainActivity.java
@@ -3,6 +3,7 @@ package app.movil.primeraapp3008;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"OnCreate",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Toast.makeText(this,"Onstart",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Toast.makeText(this,"Onresume",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Toast.makeText(this,"Onstop",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Toast.makeText(this,"OnDestroy",Toast.LENGTH_SHORT).show();
     }
 }
